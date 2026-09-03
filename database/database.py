ce235c41997f8abc8ad70dbe0fbd10aa12331d7f
vedikasaklani <ivedikasaklani@gmail.com>
2026-09-04 03:53:06 +0530

    moving to a render database

--- database/database.py
+++ database/database.py
@@ -5,7 +5,7 @@ from fastapi import Depends
 # we keep autocommit and autoflush off: to maintain unit of work (db is not updated per operation)
 #: to maintain atomicity (if subsequent transactions are failing, the prior ones should not execute.
 # so we cannot execute the transactions one by one simply)
-db_url="postgresql://postgres:vedika123@127.0.0.1:5433/my-fast-api-db"
+db_url="postgresql://vedika:68jAGrSDa6EVWXtpUgsw0edlu8fT0RS6@dpg-dacv4oafngtc73di14cg-a/rzp_u5f5"
 engine=create_engine(db_url)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
