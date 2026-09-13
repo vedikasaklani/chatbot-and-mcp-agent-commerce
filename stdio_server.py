f7ecb0e11dba4df477894d12a1054a557af35dbc
Vedika Saklani <ivedikasaklani@gmail.com>
2026-09-14 02:49:34 +0530

    Update stdio_server.py

--- stdio_server.py
+++ stdio_server.py
@@ -5,7 +5,4 @@ from starlette.middleware import Middleware
 from external.mcp_server import StripTrailingSlashMiddleware, mcp
 
 if __name__ == "__main__":
-    mcp.run(transport="stdio",
-            host="0.0.0.0",
-                    port=int(os.environ.get("PORT", "9000")),
-                    middleware=[Middleware(StripTrailingSlashMiddleware)])
+    mcp.run(transport="stdio")
